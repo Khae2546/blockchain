4589bfd9c51d39ddf4c75f66f034a12293cfac8d
Khae2546 <653070310111@npu.ac.th>
2024-07-23 16:05:26 +0700

    สร้าง contract ใหม่

---
+++ tips.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract tips{
+    address owner;
+    constructor(){
+        owner = msg.sender;
+    }
+
+    //1.ใส่เงินเข้าไปในกระปุก
+
+    //2.สามารถดูเงินในกระปุกได้ว่ามีเท่าไหร่
+
+    //3.เพิ่มคนที่จะได้รับทิป
+
+    //4.ลบคนได้
+
+    //5.ดูคนที่ได้รับเงิน
+
+    //6.จ่ายเงิน
+}
