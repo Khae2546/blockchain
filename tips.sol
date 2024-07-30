480790471b2da2bcad177e03167d3ece51d3bf81
Khae2546 <653070310111@npu.ac.th>
2024-07-30 15:24:32 +0700

    สร้างฟังก์ชั่นเติมเงินและดูยอดเงิน

--- tips.sol
+++ tips.sol
@@ -9,9 +9,19 @@ contract tips{
 
     //1.ใส่เงินเข้าไปในกระปุก
 
+    function addtips() payable public {
+
+    }
+
     //2.สามารถดูเงินในกระปุกได้ว่ามีเท่าไหร่
 
+    function viewtips() public view returns (uint){
+        return address(this).balance;
+    }
+
     //3.เพิ่มคนที่จะได้รับทิป
+
+
 
     //4.ลบคนได้
 
