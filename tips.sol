a496f2526480f30d2a1f72d217637e0f5bd1f97c
Khae2546 <653070310111@npu.ac.th>
2024-08-20 16:37:15 +0700

    เพิ่มคนดูเงิน/จ่ายเงิน

--- tips.sol
+++ tips.sol
@@ -31,6 +31,7 @@ contract tips{
 
     //3.2 add waitress
     function addWaitress(address payable  walletAddress,string memory name)public {
+        require(msg.sender == owner,"Only the owner can call this function");
         bool waitressExist = false;
 
         if(waitress.length >=1){
@@ -49,10 +50,39 @@ contract tips{
 }
     //4.ลบคนได้
 
+    function removeWaitress(address payable walletAddress) public {
+        if(waitress.length>=1){
+            for (uint i=0; i<waitress.length; i++){
+                if(waitress[i].walletAddress==walletAddress){
+                    for(uint j=i; j<waitress.length-1; j++){
+                        waitress[j]=waitress[j+1];
+                    }
+                        waitress.pop();
+                        break;
+                }
+            }
+        }
+    }
+
     //5.ดูคนที่ได้รับเงิน
     function viewWaitress() public  view returns  (Waitress[] memory ){
         return waitress;
     }
 
     //6.จ่ายเงิน
-}
+
+     function distrubiteTips() public {
+        require(address(this).balance >0, "Insufficient balance in the contract");
+        if(waitress.length>=1){
+            uint amount = address(this).balance / waitress.length;
+            for (uint i=0; i<waitress.length; i++){
+                transfer(waitress[i].walletAddress,amount);
+            }
+        }
+
+    }
+        //transfer money
+        function transfer(address payable walletAddress,uint amount) internal {
+            walletAddress.transfer(amount);
+        }
+}   
